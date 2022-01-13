6722b24dd0f727a13779cbbe902d1ca5f351d62d
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-01-13 19:42:09 +0700

    feat(tailwind.config.js, HeaderComponent, _app.tsx, globals.css): :sparkles: add base dark mode

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,21 +1,32 @@
 import clsx from "clsx";
+import { useTheme } from "next-themes";
 import React from "react";
-import { BiSun } from "react-icons/bi";
+import { BiMoon, BiSun } from "react-icons/bi";
 
 const HeaderComponent = () => {
+  const { theme, setTheme } = useTheme();
+
+  const handleChangeTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
-    <header className="fixed top-0 z-50 bg-gray-200 w-full sm:p-1 md:p-2 opacity-90">
+    <header className="fixed top-0 z-50 bg-gray-200 dark:bg-gray-800 w-full sm:p-1 md:p-2 opacity-90">
       <div className="layout flex justify-between items-center h-14 max-w-4xl mx-auto px-2 md:px-1">
-        <h5 className="font-bold">yehezgun.com</h5>
+        <h5 className="font-bold text-black dark:text-white">yehezgun.com</h5>
         <button
           className={clsx(
             "p-2.5 rounded-md focus:outline-none",
-            "border dark:border-gray-600",
+            "border border-gray-900 dark:border-gray-600",
             "dark:hover:border-primary-300 dark:hover:text-primary-300 hover:text-primary-300 hover:border-primary-300",
             "dark:focus-visible:border-primary-300 dark:focus-visible:text-primary-300 focus-visible:text-primary-300 focus-visible:border-primary-300"
           )}
         >
-          <BiSun size={20} />
+          {theme === "light" ? (
+            <BiSun size={20} onClick={() => handleChangeTheme()} />
+          ) : (
+            <BiMoon size={20} onClick={() => handleChangeTheme()} />
+          )}
         </button>
       </div>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
