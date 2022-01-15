5550006a1790e606f155bc43192b36fecda0888c
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-01-15 18:42:02 +0700

    style(HeaderComponent): :lipstick: fix the dark mode button

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -11,7 +11,7 @@ const HeaderComponent = () => {
   };
 
   return (
-    <header className="fixed top-0 z-50 bg-gray-200 dark:bg-gray-800 w-full sm:p-1 md:p-2 opacity-90">
+    <header className="fixed top-0 z-50 bg-gray-200 dark:bg-gray-800 w-full p-1 opacity-90">
       <div className="layout flex justify-between items-center h-14 max-w-4xl mx-auto px-2 md:px-1">
         <h5 className="font-bold text-black dark:text-white">yehezgun.com</h5>
         <button
@@ -21,12 +21,9 @@ const HeaderComponent = () => {
             "dark:hover:border-primary-300 dark:hover:text-primary-300 hover:text-primary-300 hover:border-primary-300",
             "dark:focus-visible:border-primary-300 dark:focus-visible:text-primary-300 focus-visible:text-primary-300 focus-visible:border-primary-300"
           )}
+          onClick={() => handleChangeTheme()}
         >
-          {theme === "light" ? (
-            <BiSun size={20} onClick={() => handleChangeTheme()} />
-          ) : (
-            <BiMoon size={20} onClick={() => handleChangeTheme()} />
-          )}
+          {theme === "light" ? <BiSun size={20} /> : <BiMoon size={20} />}
         </button>
       </div>
     </header>
