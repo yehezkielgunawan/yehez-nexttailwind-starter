1873c532eafc13af94234a5acff10419b1c72ff1
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-05-12 20:44:16 +0700

    style(BaseImage and NextImage): :sparkles: set images component folder

---
+++ src/components/images/BaseImage.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+import clsxm from "@/lib/helpers/clsxm";
+
+type BaseImageProps = {
+  useSkeleton?: boolean;
+  className?: string;
+  imgClassName?: string;
+} & React.ComponentPropsWithRef<"img">;
+
+const BaseImage = React.forwardRef<HTMLImageElement, BaseImageProps>(
+  ({ useSkeleton = false, className, imgClassName, ...rest }, ref) => {
+    const [status, setStatus] = useState<string>(
+      useSkeleton ? "loading" : "complete"
+    );
+    return (
+      <figure ref={ref} className={clsxm(className)}>
+        <img
+          src={clsxm(imgClassName, status === "loading" && "animate-pulse")}
+          alt={rest.alt}
+          onLoad={() => setStatus("complete")}
+          {...rest}
+        />
+      </figure>
+    );
+  }
+);
+
+BaseImage.displayName = "BaseImage";
+
+export default BaseImage;
