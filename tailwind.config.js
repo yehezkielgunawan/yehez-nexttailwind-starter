72d1610166b23706e67170039470c4ab07d7d53f
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-05-12 20:43:15 +0700

    chore(.eslintrc, tailwind.config, and global.css): :lipstick: remove unused styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
-function withOpacityValue(variable) {
-  return ({ opacityValue }) => {
-    if (opacityValue === undefined) {
-      return `rgb(var(${variable}))`;
-    }
-    return `rgb(var(${variable}) / ${opacityValue})`;
-  };
-}
-
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
@@ -20,17 +11,16 @@ module.exports = {
       },
       colors: {
         primary: {
-          // Customize it on globals.css :root
-          50: withOpacityValue("--tw-color-primary-50"),
-          100: withOpacityValue("--tw-color-primary-100"),
-          200: withOpacityValue("--tw-color-primary-200"),
-          300: withOpacityValue("--tw-color-primary-300"),
-          400: withOpacityValue("--tw-color-primary-400"),
-          500: withOpacityValue("--tw-color-primary-500"),
-          600: withOpacityValue("--tw-color-primary-600"),
-          700: withOpacityValue("--tw-color-primary-700"),
-          800: withOpacityValue("--tw-color-primary-800"),
-          900: withOpacityValue("--tw-color-primary-900"),
+          50: "#E7F6FE",
+          100: "#CFEEFC",
+          200: "#9ADBF9",
+          300: "#6ACAF6",
+          400: "#3AB8F3",
+          500: "#0EA5E9",
+          600: "#0B84BC",
+          700: "#08628C",
+          800: "#05405B",
+          900: "#032230",
         },
         dark: "#222222",
       },
