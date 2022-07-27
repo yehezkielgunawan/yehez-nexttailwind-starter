dfa4f32dfac202cd1904beebf05fcc606e9b1e75
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2022-07-27 20:06:31 +0700

    test(__test__): :white_check_mark: update unit test for 404 page

--- src/__test__/index.test.tsx
+++ src/__test__/index.test.tsx
@@ -4,10 +4,11 @@ import { render, screen } from "@testing-library/react";
 
 import Home from "../pages/index";
 
+import NotFoundPage from "@/pages/404";
 import "@testing-library/jest-dom";
 import Components from "@/pages/components";
 
-describe("Home", () => {
+describe("Render Page Normally", () => {
   it("Renders Home Page Successfully", () => {
     render(<Home />);
     const heading = screen.getByText("Hello World!");
@@ -17,6 +18,11 @@ describe("Home", () => {
   it("Renders Components Page Successfully", () => {
     render(<Components />);
     const heading = screen.getByText("Reusable Components");
+    expect(heading).toBeInTheDocument();
+  });
+  it("Renders 404 page successfully!", () => {
+    render(<NotFoundPage />);
+    const heading = screen.getByText("Page Not Found");
     expect(heading).toBeInTheDocument();
   });
 });
